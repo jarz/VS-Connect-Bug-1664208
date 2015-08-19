1a14a0f295d7974b3d6b1061aa83aecebfa27444
Tim McGee <tmcgee@t2systems.com>
2015-08-19 16:27:37 -0500

    Fakes won't generate a shim for SampleClass.ReturnsAMemoryStream() in VS 2013 Update 5 or VS 2015 RTM.

---
+++ VS Connect Bug 1664208/SampleClass/SampleClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAssembly
+{
+    public class SampleClass
+    {
+        public MemoryStream ReturnsAMemoryStream()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write("SampleClass Memory Stream");
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ VS Connect Bug 1664208/SampleTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleAssembly;
+using SampleAssembly.Fakes;
+
+namespace SampleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (ShimsContext.Create())
+            {
+                const string fakeStr = "Faked Memory Stream";
+
+                ShimSampleClass.AllInstances.ReturnsAMemoryStream = (@this) =>
+                    {
+                        var stream = new MemoryStream();
+                        var writer = new StreamWriter(stream);
+                        writer.Write(fakeStr);
+                        writer.Flush();
+                        stream.Position = 0;
+                        return stream;
+                    };
+
+                var sampleObj = new SampleClass();
+                using (var sampleStream = sampleObj.ReturnsAMemoryStream())
+                using (var reader = new StreamReader(sampleStream))
+                {
+                    var returnedString = reader.ReadToEnd();
+                    Assert.AreEqual(fakeStr, returnedString);
+                }
+            }
+        }
+    }
+}
